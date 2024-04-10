fe64016c9b5736d39c29bb90f7c7d7b82446c386
Santiago Pintos <spintos@outlook.es>
2024-04-10 16:42:27 -0300

    LogicaAplicacion: Se implementa CrearUsuario
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ Tienda.LogicaAplicacion/CasosDeUso/Usuario/CrearUsuarioCU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.Usuario;
+using Tienda.LogicaAplicacion.Mappers;
+using Tienda.LogicaNegocio.InterfacesRepositorio;
+
+namespace Tienda.LogicaAplicacion.CasosDeUso.Usuario
+{
+    public class CrearUsuarioCU : ICreateUsuario
+    {
+        private IRepositorioUsuario _repositorioUsuario;
+        public CrearUsuarioCU(IRepositorioUsuario repositorioUsuario)
+        {
+            this._repositorioUsuario = repositorioUsuario;
+        }
+
+        public void CrearUsuario(UsuarioDTO aCrear)
+        {
+            this._repositorioUsuario.Add(UsuarioDtoMapper.FromDto(aCrear));
+        }
+    }
+}
