68760dbeb9d3f3904a5b5cb1c1faea6d1978a61e
Santiago Pintos <spintos@outlook.es>
2024-04-09 22:01:51 -0300

    Articulo: se implementan validaciones en EsValido
    * TODO: Verificar validación de cifras significativas de código.
    
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- Tienda.LogicaNegocio/Entidades/Articulo.cs
+++ Tienda.LogicaNegocio/Entidades/Articulo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tienda.LogicaNegocio.Excepciones.Articulo;
 using Tienda.LogicaNegocio.InterfacesEntidades;
 
 namespace Tienda.LogicaNegocio.Entidades
@@ -14,11 +15,36 @@ namespace Tienda.LogicaNegocio.Entidades
         public int Codigo { get; set; }
         public string Descripcion { get; set; }
         public int stock { get; set; }
-        public string PrecioUnitario { get; set; }
+        public decimal PrecioUnitario { get; set; }
 
         public void EsValido()
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(Nombre))
+            {
+                throw new NombreNoValidoException("El nombre no puede ser vacío");
+            }
+            if (Descripcion.Length < 5 || string.IsNullOrEmpty(Descripcion))
+            {
+                throw new DescripcionNoValidaException("La descripción debe tener al menos 5 caracteres");
+            }
+            if (stock < 0)
+            {
+                throw new StockNoValidoException("El stock no puede ser negativo");
+            }
+            if (Descripcion.Length < 13) {
+                throw new DescripcionNoValidaException("La descripción debe tener al menos 13 caracteres");
+            }
+            if (PrecioUnitario < 0)
+            {
+                throw new PrecioUnitarioNoValidoException("El precio no es válido");
+            }
+            //el código debe tener al menos 13 digitos significativos, se convierte a string para contar
+            //la cant de digitos
+            if (Descripcion.ToString().Length<13)
+            {
+                throw new CodigoNoValidoException("El código no es válido");
+            }
+
         }
     }
 }
