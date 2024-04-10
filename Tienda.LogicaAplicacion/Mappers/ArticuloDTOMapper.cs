dd694fbbf86c0ebbe088e20b38c5d618d7e897d5
Santiago Pintos <spintos@outlook.es>
2024-04-10 13:13:08 -0300

    Se genera Mapper DTO de Articulo
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ Tienda.LogicaAplicacion/Mappers/ArticuloDTOMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaNegocio.Entidades;
+
+namespace Tienda.LogicaAplicacion.Mappers
+{
+    public class ArticuloDTOMapper
+    {
+        public static ArticuloDTO toDto(Articulo articulo)
+        {
+            return new ArticuloDTO(articulo);
+        }
+
+        public static Articulo FromDto(ArticuloDTO articuloDTO)
+        {
+            return new Articulo(articuloDTO.Id, articuloDTO.Nombre, articuloDTO.Codigo, articuloDTO.Descripcion, articuloDTO.stock, articuloDTO.PrecioUnitario);
+        }
+    }
+}
