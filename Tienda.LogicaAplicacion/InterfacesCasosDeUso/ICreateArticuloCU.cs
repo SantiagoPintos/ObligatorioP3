1420d38fc677286541a93fd8728283c6bc1525f2
Santiago Pintos <spintos@outlook.es>
2024-04-09 21:48:00 -0300

    Se crea interfaz y CU para crear artículos
    * Falta controlador
    
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ Tienda.LogicaAplicacion/InterfacesCasosDeUso/ICreateArticuloCU.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaNegocio.Entidades;
+
+namespace Tienda.LogicaAplicacion.InterfacesCasosDeUso
+{
+    public interface ICreateArticuloCU
+    {
+        public void CreateArticulo(Articulo articulo);
+    }
+}
