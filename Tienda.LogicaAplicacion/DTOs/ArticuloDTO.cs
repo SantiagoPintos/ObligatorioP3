44935e1d0ff7d3584528e0680e753d516e99080c
Santiago Pintos <spintos@outlook.es>
2024-04-10 13:10:01 -0300

    Se genera DTO de Articulo
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ Tienda.LogicaAplicacion/DTOs/ArticuloDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicaNegocio.Entidades;
+using Tienda.LogicaNegocio.Entidades;
+
+namespace Tienda.LogicaAplicacion.DTOs
+{
+    public class ArticuloDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public int stock { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+        public ArticuloDTO() { }
+        public ArticuloDTO(Articulo articulo)
+        {
+            if(articulo == null) throw new Exception();
+
+            this.Id = articulo.Id;
+            this.Nombre = articulo.Nombre;
+            this.Codigo = articulo.Codigo;
+            this.Descripcion = articulo.Descripcion;
+            this.stock = articulo.stock;
+            this.PrecioUnitario = articulo.PrecioUnitario;
+        }
+    }
+}
