13a0cb2010ae2fca9c6288ef47100a1723214583
Santiago Pintos <spintos@outlook.es>
2024-04-10 16:36:49 -0300

    LogicaAplicacion: Se implementa UsuarioDTO y Mapper
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ Tienda.LogicaAplicacion/DTOs/UsuarioDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaNegocio.Entidades;
+using Tienda.LogicaNegocio.ValueObjects;
+
+namespace Tienda.LogicaAplicacion.DTOs
+{
+    public class UsuarioDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        //letra dice que se debe guardar una copia de la contraseña sin encriptar
+        public string ClaveSinEncriptar { get; set; }
+
+        public UsuarioDTO() { }
+
+        public UsuarioDTO(Usuario usuario)
+        {
+            if(usuario == null) throw new Exception();
+
+            this.Id = usuario.Id;
+            this.Nombre = usuario.NombreCompleto.Nombre;
+            this.Apellido = usuario.NombreCompleto.Apellido;
+            this.Email = usuario.Email;
+            this.Clave = usuario.Clave;
+            this.ClaveSinEncriptar = usuario.ClaveSinEncriptar;
+        }
+    }
+}
---
+++ Tienda.LogicaAplicacion/Mappers/UsuarioDtoMapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaNegocio.Entidades;
+
+namespace Tienda.LogicaAplicacion.Mappers
+{
+    public class UsuarioDtoMapper
+    {
+        public static UsuarioDTO toDto(Usuario usuario)
+        {
+            return new UsuarioDTO(usuario);
+        }
+
+        public static Usuario FromDto(UsuarioDTO usuarioDTO)
+        {
+            if(usuarioDTO == null) throw new Exception();
+
+            return new Usuario(usuarioDTO.Nombre, usuarioDTO.Apellido, usuarioDTO.Email, usuarioDTO.Clave, usuarioDTO.ClaveSinEncriptar);
+        }
+    }
+}
