fe54a308e2e8b507162edf1344b5ac97d37aa81f
Santiago Pintos <spintos@outlook.es>
2024-04-11 13:14:16 -0300

    Se crea controlador de Usuario
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- Tienda.Web/Program.cs
+++ Tienda.Web/Program.cs
@@ -27,6 +27,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Usuario}/{action=Index}/{id?}");
 
 app.Run();
