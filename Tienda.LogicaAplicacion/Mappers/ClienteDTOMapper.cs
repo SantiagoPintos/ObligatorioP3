120c4c5afef044514e09b13c7f4c993b0a6f70a5
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-04-25 18:52:26 -0300

    ClienteDTOMapper: Se crean metodos fromDTO y toDTO

---
+++ Tienda.LogicaAplicacion/Mappers/ClienteDTOMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaNegocio.Entidades;
+using Tienda.LogicaNegocio.ValueObjects;
+
+namespace Tienda.LogicaAplicacion.Mappers
+{
+    public class ClienteDTOMapper
+    {
+
+        public static ClienteDTO toDto(Cliente cliente)
+        {
+            return new ClienteDTO(cliente);
+        }
+
+        public static Cliente FromDto(ClienteDTO clienteDTO)
+        {
+            if(clienteDTO == null) throw new Exception();            
+            return new Cliente(clienteDTO.Id, clienteDTO.Rut, clienteDTO.RazonSocial, clienteDTO.Calle, clienteDTO.Numero, clienteDTO.Ciudad, clienteDTO.distanciaDesdeTienda);
+        }
+
+
+    }
+}
