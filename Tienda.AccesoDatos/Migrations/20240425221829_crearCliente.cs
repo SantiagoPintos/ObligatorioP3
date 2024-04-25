2d70f7424adf6f77026b447eaabbb49e538cec09
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-04-25 20:07:49 -0300

    Se crean migraciones crearCliente

---
+++ Tienda.AccesoDatos/Migrations/20240425221829_crearCliente.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tienda.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class crearCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
