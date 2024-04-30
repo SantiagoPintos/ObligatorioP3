bf2a253e2251a862018e85a6300855d33506a937
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-04-30 16:26:32 -0300

    Se crea Caso de Uso EliminarUsuarioCU e interfaz

---
+++ Tienda.LogicaAplicacion/CasosDeUso/Usuario/EliminarUsuarioCU.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.Usuario;
+using Tienda.LogicaAplicacion.Mappers;
+using Tienda.LogicaNegocio.InterfacesRepositorio;
+
+namespace Tienda.LogicaAplicacion.CasosDeUso.Usuario
+{
+    public class EliminarUsuarioCU : IEliminarUsuario
+    {
+        private IRepositorioUsuario _repositorioUsuario;
+        public EliminarUsuarioCU(IRepositorioUsuario _repositorioUsuario)
+        {
+            this._repositorioUsuario = _repositorioUsuario;
+        }
+        public void EliminarUsuario(UsuarioDTO usuario)
+        {            
+            this._repositorioUsuario.Remove(UsuarioDtoMapper.FromDto(usuario));
+        }
+    }
+
+}
+
