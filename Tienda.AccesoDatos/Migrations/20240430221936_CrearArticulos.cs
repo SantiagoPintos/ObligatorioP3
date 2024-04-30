40bf974413b6944fa05f0e04d9b2eb2efcaef67a
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-04-30 20:32:44 -0300

    Migraciones: Creacion de articulos

---
+++ Tienda.AccesoDatos/Migrations/20240430221936_CrearArticulos.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tienda.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrearArticulos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Codigo",
+                table: "Articulos",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Codigo",
+                table: "Articulos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
