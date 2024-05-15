0ba31fd58cdf5174731d4c7cf5dda8edcf75e7c7
Santiago Pintos <spintos@outlook.es>
2024-05-15 15:33:52 -0300

    Se unifica pedido dto mapper todto

--- Tienda.LogicaAplicacion/Mappers/PedidoDTOMapper.cs
+++ Tienda.LogicaAplicacion/Mappers/PedidoDTOMapper.cs
@@ -12,33 +12,18 @@ namespace Tienda.LogicaAplicacion.Mappers
 {
     public class PedidoDTOMapper
     {
-        public static PedidoDTO toDtoFromComun(Comun comun)
+        public static PedidoDTO toDto(Pedido pedido)
         {
-            if (comun == null) throw new Exception();
+            if (pedido == null) throw new Exception();
             PedidoDTO pedidoDTO = new PedidoDTO();                  
-            pedidoDTO.Fecha = comun.Fecha;
-            pedidoDTO.Cliente = (ClienteDTOMapper.toDto(comun.Cliente));
-            pedidoDTO.lineas = comun.lineas.Select(linea => LineaDTOMapper.toDto(linea)).ToList();
-            pedidoDTO.PrecioTotal = comun.PrecioTotal;
-            pedidoDTO.Recargo = comun.Recargo;
-            pedidoDTO.IVA = comun.IVA;
-            pedidoDTO.FechaEntrega = comun.FechaEntrega;
-            pedidoDTO.anulado = comun.anulado;
-            return pedidoDTO;
-        }
-        public static PedidoDTO toDtoFromExpress(Express express)
-        {
-            if (express == null) throw new Exception();
-            PedidoDTO pedidoDTO = new PedidoDTO();
-            pedidoDTO.Id = express.Id;
-            pedidoDTO.Fecha = express.Fecha;
-            pedidoDTO.Cliente = (ClienteDTOMapper.toDto(express.Cliente));
-            pedidoDTO.lineas = express.lineas.Select(linea => LineaDTOMapper.toDto(linea)).ToList();
-            pedidoDTO.PrecioTotal = express.PrecioTotal;
-            pedidoDTO.Recargo = express.Recargo;
-            pedidoDTO.IVA = express.IVA;
-            pedidoDTO.FechaEntrega = express.FechaEntrega;
-            pedidoDTO.anulado = express.anulado;
+            pedidoDTO.Fecha = pedido.Fecha;
+            pedidoDTO.Cliente = (ClienteDTOMapper.toDto(pedido.Cliente));
+            pedidoDTO.lineas = pedido.lineas.Select(linea => LineaDTOMapper.toDto(linea)).ToList();
+            pedidoDTO.PrecioTotal = pedido.PrecioTotal;
+            pedidoDTO.Recargo = pedido.Recargo;
+            pedidoDTO.IVA = pedido.IVA;
+            pedidoDTO.FechaEntrega = pedido.FechaEntrega;
+            pedidoDTO.anulado = pedido.anulado;
             return pedidoDTO;
         }
 
