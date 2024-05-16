3613bb9461aa64aff8e28b3f352d1c4133ccbed6
Martin Diaz Preiero <MD303783@fi365.ort.edu.uy>
2024-05-15 23:12:03 -0300

    Se crea AnularPedidoCU

---
+++ Tienda.LogicaAplicacion/CasosDeUso/Pedido/AnularPedidoCU.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.Pedido;
+using Tienda.LogicaNegocio.InterfacesRepositorio;
+
+namespace Tienda.LogicaAplicacion.CasosDeUso.Pedido
+{
+    public class AnularPedidoCU:IAnularPedido
+    {
+        private IRepositorioPedido _repositorioPedido;
+        public AnularPedidoCU(IRepositorioPedido repositorioPedido)
+        {
+            _repositorioPedido = repositorioPedido;
+        }
+
+        public void AnularPedido(int id)
+        {
+            this._repositorioPedido.AnularPedido(id);
+        }
+    }
+}
