c5fda2257a5ba523bd6e72d36ea202d3bb59ec3c
Santiago Pintos <spintos@outlook.es>
2024-06-05 21:40:32 -0300

    Se crea Exception de Movimiento

---
+++ Tienda.LogicaNegocio/Excepciones/Movimiento/MovimientoNoValidoException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda.LogicaNegocio.Excepciones.Movimiento
+{
+    public class MovimientoNoValidoException : Exception
+    {
+        public MovimientoNoValidoException() { }
+        public MovimientoNoValidoException(string mensaje) : base(mensaje) { }
+
+        public MovimientoNoValidoException(string? mensaje, Exception? inner) : base(mensaje, inner) { }
+
+        protected MovimientoNoValidoException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
