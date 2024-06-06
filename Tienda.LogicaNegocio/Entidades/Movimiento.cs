32cd6d897f35a30a5b750e4792458c3e85558477
Santiago Pintos <spintos@outlook.es>
2024-06-05 21:40:20 -0300

    Se crea entidad Movimiento

---
+++ Tienda.LogicaNegocio/Entidades/Movimiento.cs
@@ -0,0 +1,51 @@
+﻿using Tienda.LogicaNegocio.InterfacesEntidades;
+using System.ComponentModel.DataAnnotations;
+using Tienda.LogicaNegocio.Excepciones.Movimiento;
+
+namespace Tienda.LogicaNegocio.Entidades
+{
+    public class Movimiento : IValidable<Movimiento>
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateOnly Fecha { get; set; }
+        [Required]
+        public TimeOnly Hora { get; set; }
+        [Required]
+        public Articulo Articulo { get; set; }
+        [Required]
+        public Usuario Usuario { get; set; }
+        [Required]
+        int Cantidad { get; set; }
+
+        public Movimiento() { }
+
+        public Movimiento(DateOnly fecha, TimeOnly hora, Articulo articulo, Usuario usuario, int cantidad)
+        {
+            this.Fecha = fecha;
+            this.Hora = hora;
+            this.Articulo = articulo;
+            this.Usuario = usuario;
+            this.Cantidad = cantidad;
+            EsValido();
+        }
+
+        public Movimiento(int id, DateOnly fecha, TimeOnly hora, Articulo articulo, Usuario usuario, int cantidad)
+        {
+            this.Id = id;
+            this.Fecha = fecha;
+            this.Hora = hora;
+            this.Articulo = articulo;
+            this.Usuario = usuario;
+            this.Cantidad = cantidad;
+            EsValido();
+        }
+
+        public void EsValido()
+        {
+            if(this.Cantidad<0) throw new MovimientoNoValidoException("La cantidad no puede ser negativa");
+            if(this.Cantidad>this.Articulo.stock) throw new MovimientoNoValidoException("La cantidad no puede ser mayor al stock");
+        }
+    }
+}
