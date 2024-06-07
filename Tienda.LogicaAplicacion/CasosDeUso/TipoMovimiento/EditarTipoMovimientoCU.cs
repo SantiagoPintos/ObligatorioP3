e5d823fc5bed2395372b1834313bfd66a1a16a6f
Santiago Pintos <spintos@outlook.es>
2024-06-07 15:17:02 -0300

    LogicaAplicacion: Se crea caso de uso EditarTipoMovimientoCU

---
+++ Tienda.LogicaAplicacion/CasosDeUso/TipoMovimiento/EditarTipoMovimientoCU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.TipoMovimiento;
+using Tienda.LogicaAplicacion.Mappers;
+using Tienda.LogicaNegocio.InterfacesRepositorio;
+
+namespace Tienda.LogicaAplicacion.CasosDeUso.TipoMovimiento
+{
+    public class EditarTipoMovimientoCU : IEditarTipoMovimiento
+    {
+        private IRepositorioTipoMovimiento _repositorio;
+        public EditarTipoMovimientoCU(IRepositorioTipoMovimiento repositorio)
+        {
+            this._repositorio = repositorio;
+        }
+
+        public void EditarTipoMovimiento(TipoMovimientoDTO aEditar)
+        {
+            this._repositorio.Update(TipoMovimientoDTOMapper.fromDTO(aEditar));
+        }
+    }
+}
