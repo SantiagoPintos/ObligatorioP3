930b33bef4b7ee8ffd05e9fc9a5e78c44b66c6e1
Santiago Pintos <spintos@outlook.es>
2024-06-07 15:22:17 -0300

    LogicaAplicacion: Se crea caso de uso ListarTipoMovimientoCU

---
+++ Tienda.LogicaAplicacion/CasosDeUso/TipoMovimiento/ListarTipoMovimientoCU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.TipoMovimiento;
+using Tienda.LogicaAplicacion.Mappers;
+using Tienda.LogicaNegocio.InterfacesRepositorio;
+
+namespace Tienda.LogicaAplicacion.CasosDeUso.TipoMovimiento
+{
+    public class ListarTipoMovimientoCU:IListarTipoMovimiento
+    {
+        private IRepositorioTipoMovimiento _repositorioTipoMovimiento;
+        public ListarTipoMovimientoCU(IRepositorioTipoMovimiento repositorioTipoMovimiento)
+        {
+            this._repositorioTipoMovimiento = repositorioTipoMovimiento;
+        }
+
+        public IEnumerable<TipoMovimientoDTO> ListarTipoMovimiento()
+        {
+            return this._repositorioTipoMovimiento.FindAll().Select(tipo => TipoMovimientoDTOMapper.toDTO(tipo));
+        }
+    }
+}
