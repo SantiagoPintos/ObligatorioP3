2f8b59e7e8155fa3125f9b2f8d9d07c4e5e7a813
Santiago Pintos <spintos@outlook.es>
2024-06-07 15:29:29 -0300

    LogicaAplicacion: Se crea caso de uso EliminarTipoMovimientoCU

---
+++ Tienda.LogicaAplicacion/CasosDeUso/TipoMovimiento/EliminarTipoMovimientoCU.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.TipoMovimiento;
+using Tienda.LogicaAplicacion.Mappers;
+using Tienda.LogicaNegocio.Excepciones.TipoMovimiento;
+using Tienda.LogicaNegocio.InterfacesRepositorio;
+
+namespace Tienda.LogicaAplicacion.CasosDeUso.TipoMovimiento
+{
+    public class EliminarTipoMovimientoCU:IEliminarTipoMovimiento
+    {
+        private IRepositorioTipoMovimiento _repositorio;
+        public EliminarTipoMovimientoCU(IRepositorioTipoMovimiento repositorio)
+        {
+            this._repositorio = repositorio;
+        }
+
+        public void EliminarTipoMovimiento(TipoMovimientoDTO tipo)
+        {
+            if(tipo==null) throw new TipoMovimientoNoValidoException("El movimiento a eliminar no es válido");
+            this._repositorio.Remove(TipoMovimientoDTOMapper.fromDTO(tipo));
+        }
+    }
+}
