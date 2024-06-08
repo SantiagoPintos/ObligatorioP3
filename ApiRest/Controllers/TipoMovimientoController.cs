fec261b879c4ec74a91ef017425cdeb547159d3b
Santiago Pintos <spintos@outlook.es>
2024-06-08 17:23:49 -0300

    LogicaAplicacion: EditarTipoMovimientoCU: Solo editar movimiento si no está en uso

--- ApiRest/Controllers/TipoMovimientoController.cs
+++ ApiRest/Controllers/TipoMovimientoController.cs
@@ -137,7 +137,6 @@ namespace ApiRest.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<TipoMovimientoDTO> Update(int id, [FromBody] TipoMovimientoDTO tipoMovimiento)
         {
-            //Falta verificar que no se estén utilizando en algún movimiento antes de ser actualizados
             try
             {
                 //Evita que id de DTO sea distinto al id de la URL y/o que tenga id 0
