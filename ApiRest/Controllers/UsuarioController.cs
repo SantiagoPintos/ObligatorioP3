6ac468f631a2e7550339f2f22e3d819d6cd7b451
Santiago Pintos <spintos@outlook.es>
2024-06-09 22:26:23 -0300

    ApiRest: Se implementa UsuarioController con login

---
+++ ApiRest/Controllers/UsuarioController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.Usuario;
+using Tienda.LogicaNegocio.Excepciones.Usuario;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private ILoginUsuario _login;
+        public UsuarioController(ILoginUsuario login)
+        {
+            _login = login;
+        }
+        // POST api/<UsuarioController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Post([FromBody] UsuarioDTO usuario)
+        {
+            try
+            {
+                _login.Login(usuario);
+                return Ok("Logueado con éxito");
+            }
+            catch (UsuarioNoValidoException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                //preguntar por 500 acá
+                return BadRequest();
+            }
+        }
+    }
+}
