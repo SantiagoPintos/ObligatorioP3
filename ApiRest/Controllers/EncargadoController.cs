bc546d38b466e5f094f6dad8c98dbc80602388ca
Santiago Pintos <spintos@outlook.es>
2024-06-10 21:57:35 -0300

    ApiRest: Se implementa endpoint para login de Encargado

---
+++ ApiRest/Controllers/EncargadoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.Encargado;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaNegocio.Excepciones.Encargado;
+
+namespace ApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EncargadoController : ControllerBase
+    {
+        private ILoginEncargado _login;
+        public EncargadoController(ILoginEncargado login)
+        {
+            _login = login;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult Post([FromBody] EncargadoDTO encargado)
+        {
+            try
+            {
+                _login.login(encargado);
+                return Ok("Logueado con éxito");
+            }
+            catch (EncargadoException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Algo salió mal");
+            }
+        }
+    }
+}
