95696359fd89eadf77140a0e10340837e18e6487
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-06-13 21:33:17 -0300

    LoginEncargadoCU: Se verifica la existencia de token, se elimina o se agrega uno nuevo.

--- Tienda.LogicaAplicacion/CasosDeUso/Encargado/LoginEncargadoCU.cs
+++ Tienda.LogicaAplicacion/CasosDeUso/Encargado/LoginEncargadoCU.cs
@@ -10,6 +10,7 @@ using Tienda.LogicaNegocio.InterfacesRepositorio;
 using Tienda.LogicaNegocio.Excepciones;
 using Tienda.LogicaNegocio.Excepciones.Encargado;
 using System.Security.Principal;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.Token;
 
 namespace Tienda.LogicaAplicacion.CasosDeUso.Encargado
 {
@@ -17,10 +18,16 @@ namespace Tienda.LogicaAplicacion.CasosDeUso.Encargado
     {
         private IRepositorioEncargado _repositorioEncargado;
         private IObtenerEncargadoPorEmail _obtenerEncargadoPorEmail;
-        public LoginEncargadoCU(IRepositorioEncargado repositorioEncargado, IObtenerEncargadoPorEmail encargadoPorEmail)
+        private IEncontrarTokenPorEmail _encontrarTokenPorEmail;
+        private IEliminarToken _eliminarToken;
+        private IAlmacenarToken _almacenarToken;
+        public LoginEncargadoCU(IRepositorioEncargado repositorioEncargado, IObtenerEncargadoPorEmail encargadoPorEmail, IEncontrarTokenPorEmail encontrarTokenPorEmail, IEliminarToken eliminarToken, IAlmacenarToken almacenarToken)
         {
             this._repositorioEncargado = repositorioEncargado;
             this._obtenerEncargadoPorEmail = encargadoPorEmail;
+            this._encontrarTokenPorEmail = encontrarTokenPorEmail;
+            _eliminarToken = eliminarToken;
+            _almacenarToken = almacenarToken;
         }
         public string login(EncargadoDTO encargado)
         {
@@ -28,6 +35,16 @@ namespace Tienda.LogicaAplicacion.CasosDeUso.Encargado
             if(string.IsNullOrEmpty(encargado.Email) || string.IsNullOrEmpty(encargado.Clave)) throw new EncargadoException("Datos incorrectos");
             this._repositorioEncargado.Login(encargado.Email, encargado.Clave);
             string token = ManejadorJwt.GenerarToken(_obtenerEncargadoPorEmail.ObtenerEncargadoPorEmail(encargado.Email));
+            // Verificar que no exista un token para este usuario, si es asi, eliminarlo, guardar el generado y luego retornarlo
+            TokenDTO tokenBuscado = this._encontrarTokenPorEmail.EncontrarTokenPorEmail(encargado.Email);
+            if(tokenBuscado != null) this._eliminarToken.EliminarToken(tokenBuscado.TokenUsuario);
+            // Creamos nuevo tokenDTO
+            TokenDTO tokenNuevo = new TokenDTO
+            {
+                TokenUsuario = token,
+                Encargado = encargado
+            };           
+            _almacenarToken.almacenarToken(tokenNuevo);
             return token;
         }
     }
