45b4cd513d8a785b24e73840c3cdcab235cbfd06
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-06-13 21:34:19 -0300

    Creacion de tokenDTO

---
+++ Tienda.LogicaAplicacion/DTOs/TokenDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaNegocio.Entidades;
+
+namespace Tienda.LogicaAplicacion.DTOs
+{
+    public class TokenDTO
+    {
+        public int Id { get; set; }
+        public string TokenUsuario { get; set; }
+        public EncargadoDTO Encargado { get; set; }
+    }
+}
