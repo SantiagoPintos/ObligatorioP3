322f21142e54bbd663512530a48692a77529fec7
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-06-13 21:34:44 -0300

    Interfaz de CU Eliminar token

---
+++ Tienda.LogicaAplicacion/InterfacesCasosDeUso/Token/IEliminarToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda.LogicaAplicacion.InterfacesCasosDeUso.Token
+{
+    public interface IEliminarToken
+    {
+        public void EliminarToken(string email);
+    }
+}
