1aac273f24a9fb9a91eff7cf4742205e060221c6
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-06-13 21:34:12 -0300

    ObtenerTokenPorEmailCU: Creacion de CU para obtener token por email de encargado.

---
+++ Tienda.LogicaAplicacion/CasosDeUso/Token/ObtenerTokenPorEmailCU.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.Token;
+using Tienda.LogicaAplicacion.Mappers;
+using Tienda.LogicaNegocio.InterfacesRepositorio;
+
+namespace Tienda.LogicaAplicacion.CasosDeUso.Token
+{
+    public class ObtenerTokenPorEmailCU : IEncontrarTokenPorEmail
+    {
+        private IRepositorioToken _repositorioToken;
+        public ObtenerTokenPorEmailCU(IRepositorioToken repositorioToken)
+        {
+            this._repositorioToken = repositorioToken;
+        }
+        public TokenDTO EncontrarTokenPorEmail(string email)
+        {
+            return TokenDTOMapper.toDto(this._repositorioToken.FindByEmail(email));
+        }
+    }
+}
