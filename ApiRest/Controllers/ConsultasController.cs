7fb537b550590939fd94baa42cb57de435b1ae38
Santiago Pintos <spintos@outlook.es>
2024-06-15 11:39:45 -0300

    ApiRest: ConsultasController: Se agrega ActionResult para RF04 A

--- ApiRest/Controllers/ConsultasController.cs
+++ ApiRest/Controllers/ConsultasController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Tienda.LogicaAplicacion.DTOs;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.Movimiento;
 
 namespace ApiRest.Controllers
 {
@@ -7,5 +9,43 @@ namespace ApiRest.Controllers
     [ApiController]
     public class ConsultasController : ControllerBase
     {
+        private IObtenerMovimientosSobreArticulo obtenerMovimientosSobreArticulo;
+        public ConsultasController(IObtenerMovimientosSobreArticulo obtenerMovimientosSobreArticulo)
+        {
+            this.obtenerMovimientosSobreArticulo = obtenerMovimientosSobreArticulo;
+        }
+
+
+        /// <summary>
+        /// RF-04 a - Obtiene los movimientos de un articulo en base a su id y tipo de movimiento
+        /// </summary>
+        /// <param name="idArticulo"></param>
+        /// <param name="tipoMovimiento"></param>
+        /// <returns></returns>
+        [Route("MovimientosIdTipo/{idArticulo}/{tipoMovimiento}")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<IEnumerable<MovimientoDTO>> ObtenerMovimientos(int idArticulo, string tipoMovimiento)
+        {
+            try
+            {
+                IEnumerable<MovimientoDTO> listaDeDTO = obtenerMovimientosSobreArticulo.ObtenerMovimientos(idArticulo, tipoMovimiento);
+                if (listaDeDTO.Count() > 0)
+                {
+                    return Ok(listaDeDTO);
+                }
+                else
+                {
+                    return NoContent();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
