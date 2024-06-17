57f3fd93bddfd81b83b0d17968dba474082555e3
Santiago Pintos <spintos@outlook.es>
2024-06-17 10:04:14 -0300

    Se agrega migración

---
+++ Tienda.AccesoDatos/Migrations/20240617130352_eliminacionRepoToken.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tienda.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class eliminacionRepoToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tokens");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EncargadoId = table.Column<int>(type: "int", nullable: false),
+                    TokenUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tokens_Encargado_EncargadoId",
+                        column: x => x.EncargadoId,
+                        principalTable: "Encargado",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_EncargadoId",
+                table: "Tokens",
+                column: "EncargadoId");
+        }
+    }
+}
