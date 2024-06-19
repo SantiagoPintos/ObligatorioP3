d7ecb4d1e21d708cb5c2d5a9d01d52bddcba104a
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-06-18 21:41:01 -0300

    Se crea CU de ObtenerPaginado

---
+++ Tienda.LogicaAplicacion/CasosDeUso/Settings/ObtenerPaginadoCU.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda.LogicaAplicacion.InterfacesCasosDeUso.Settings;
+using Tienda.LogicaNegocio.InterfacesRepositorio;
+
+namespace Tienda.LogicaAplicacion.CasosDeUso.Settings
+{
+    public class ObtenerPaginadoCU : IObtenerPaginado
+    {
+        private IRepositorioSettings _repositorioSettings;
+        public ObtenerPaginadoCU(IRepositorioSettings repositorioSettings)
+        {
+            _repositorioSettings = repositorioSettings;
+        }
+        public double obtenerPaginado()
+        {
+            return _repositorioSettings.ObtenerPaginado();
+        }
+    }
+}
